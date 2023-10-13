f2a960e4aa9bc2e142a4c1a36867d7ef2e76e37d
koojiny <msj09252@gmail.com>
2023-10-13 16:38:59 +0900

    First commit : Upload Project File

---
+++ src/main/java/com/donothing/swithme/SwithmeApplication.java
@@ -0,0 +1,13 @@
+package com.donothing.swithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwithmeApplication.class, args);
+	}
+
+}
