32c4f56bcb13e683293ffd762958eb6b7c5f4c95
hye0e <nanda5050@naver.com>
2023-11-26 17:38:12 +0900

    Refactor: 스터디 상세 조회 1. Lazy 로딩에서 Eager 로딩으로 수정 2. MemberInfoResponDto 추가 (불필요한 멤버 정보 삭제) 3. Jsonformat 이용하여 날짜 포맷팅

---
+++ src/main/java/com/donothing/swithme/dto/study/StudyUpdateReqeustDto.java
@@ -0,0 +1,11 @@
+package com.donothing.swithme.dto.study;
+
+public class StudyUpdateReqeustDto {
+    private String title;
+    private String studyType;
+    private String numberOfMembers;
+    private String studyInfo;
+    private String studyStatus;
+    private String dateStudyStart;
+    private String dateStudyEnd;
+}
