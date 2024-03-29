406998ec7882b9788e026a8cda83267dd35272b3
hye0e <hye0e@naver.com>
2024-03-29 16:10:57 +0900

    [fix] 북마크 등록 시 memberId hidden 처리

--- src/main/java/com/donothing/swithme/dto/bookmark/BookmarkRegisterRequestDto.java
+++ src/main/java/com/donothing/swithme/dto/bookmark/BookmarkRegisterRequestDto.java
@@ -3,6 +3,7 @@ package com.donothing.swithme.dto.bookmark;
 import com.donothing.swithme.domain.Bookmark;
 import com.donothing.swithme.domain.Member;
 import com.donothing.swithme.domain.Study;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,7 +17,7 @@ public class BookmarkRegisterRequestDto {
     @NotNull(message = "북마크로 등록 될 스터디 아이디는 필수입니다.")
     private Long studyId;
 
-    // TODO: 안보이게 설정하기
+    @ApiModelProperty(hidden = true)
     private Long memberId;
 
     public Bookmark toEntity() {
