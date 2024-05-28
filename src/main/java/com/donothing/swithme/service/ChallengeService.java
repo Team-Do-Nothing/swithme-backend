8272402b28e946d0e6e1e5091ae920e2041e62fd
hye0e <nanda5050@naver.com>
2024-05-28 21:47:38 +0900

    Fix: 유효성 로직 추가

--- src/main/java/com/donothing/swithme/service/ChallengeService.java
+++ src/main/java/com/donothing/swithme/service/ChallengeService.java
@@ -1,13 +1,16 @@
 package com.donothing.swithme.service;
 
 import com.donothing.swithme.domain.Challenge;
+import com.donothing.swithme.domain.Study;
 import com.donothing.swithme.dto.challenge.ChallengeRegisterRequestDto;
 import com.donothing.swithme.dto.challenge.ChallengeRegisterResponseDto;
 import com.donothing.swithme.repository.ChallengeRepository;
 import com.donothing.swithme.repository.MemberChallengeRepository;
+import com.donothing.swithme.repository.StudyRepository;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,8 +21,17 @@ import org.springframework.stereotype.Service;
 public class ChallengeService {
     private final ChallengeRepository challengeRepository;
     private final MemberChallengeRepository memberChallengeRepository;
+    private final StudyRepository studyRepository;
 
     public ChallengeRegisterResponseDto registChallenge(ChallengeRegisterRequestDto request) {
+        Study study = studyRepository.findById(request.getStudyId()).orElseThrow(() -> {
+            log.error("존재하지 않는 스터디 입니다. studyId = " + request.getStudyId());
+            return new NoSuchElementException("존재하지 않는 스터디 입니다. ");
+        });
+
+        if (request.getMemberId() != (study.getMember().getMemberId()))
+             throw new IllegalStateException("스터디 방장만 챌린지를 개설할 수 있습니다. ");
+
         validationDate(request.getStartDate());
 
         Challenge challenge = challengeRepository.save(request.toEntity());
