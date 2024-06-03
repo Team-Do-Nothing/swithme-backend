b2f78570dc1a79f52e6904e50150c827dbb02db0
koojiny <msj09252@gmail.com>
2024-06-03 21:53:33 +0900

    Fix: 챌린지 관련 API 오타 수정

--- src/main/java/com/donothing/swithme/controller/ChallengeController.java
+++ src/main/java/com/donothing/swithme/controller/ChallengeController.java
@@ -30,18 +30,19 @@ public class ChallengeController {
 
     @PostMapping
     @ApiOperation(value = "챌린지 생성하기", notes = "챌린지를 생성하는 API 입니다.")
-    public ResponseEntity<ResponseDto<ChallengeRegisterResponseDto>> registChallenge(@RequestBody @Valid
+    public ResponseEntity<ResponseDto<ChallengeRegisterResponseDto>> registerChallenge(@RequestBody @Valid
     ChallengeRegisterRequestDto request,
             @AuthenticationPrincipal UserDetails user) {
         request.setMemberId(Long.valueOf(user.getUsername()));
         return new ResponseEntity<>(new ResponseDto<>(201, "챌린지 생성 성공",
-                challengeService.registChallenge(request)),
+                challengeService.registerChallenge(request)),
                 HttpStatus.CREATED);
     }
 
     @GetMapping("/{challengeId}")
-    @ApiOperation(value = "챌린지 조회하기", notes = "챌린지를 조회하는 API 입니다.")
-    public ResponseEntity<ResponseDto<ChallengeDetailResponseDto>> getDetailChallenge(@PathVariable String challengeId) {
+    @ApiOperation(value = "챌린지 상세 조회하기", notes = "챌린지를 상세 조회하는 API 입니다.")
+    public ResponseEntity<ResponseDto<ChallengeDetailResponseDto>> getDetailChallenge(@PathVariable String challengeId)
+    {
         return new ResponseEntity<>(new ResponseDto<>(200, "챌린지 조회 성공",
                 challengeService.detailChallengeByChallengeId(challengeId)),
                 HttpStatus.OK);
