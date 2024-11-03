db0601ccae7382dfcc99660bcb556d68e785afef
koojiny <msj09252@gmail.com>
2024-11-03 21:07:03 +0900

    Docs: security 설정 변경

--- src/main/java/com/donothing/swithme/config/SecurityConfig.java
+++ src/main/java/com/donothing/swithme/config/SecurityConfig.java
@@ -54,7 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // TO DO : 배포 전에 URI 추가 설정 필요
 //                .antMatchers("/api/v1/auth/**").permitAll()
                 .antMatchers("/**").permitAll()
-                .antMatchers("/api/v1/**").permitAll()
+                .antMatchers("/swagger-ui.html",
+                        "/configuration/security",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/webjars/**").permitAll()
 //                .anyRequest().authenticated() // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
